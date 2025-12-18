4dba37eded9936eba28dadbe3a52fd0d264b4e35
Abhishek Dhore <dhore_a@xoriant.com>
2025-12-18 17:48:53 +0530

    setting hosts

--- diary/settings.py
+++ diary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4u^52jf661+$i+wfo&#e8t^yohm4b1$_74ly3_3y+@cm-(g06^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
